f66a6b48c7c9ec4fe3ee8cd1d6ca44a2bc5f2ecc
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-11-07 19:35:24 +0300

    sml fxs, add readme

--- src/product.py
+++ src/product.py
@@ -1,19 +1,12 @@
 class Product:
+
     name: str
     description: str
     price: float
     quantity: int
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
-
-
-if __name__ == '__main__':
-    product = Product("Сыр", "Сыр с плесенью", 100.99, 3)
-
-    print(product.name)
-    print(product.description)
-    print(product.price)
-    print(product.quantity)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,19 @@
 import pytest
 
-from src.product import Product
 from src.category import Category
+from src.product import Product
 
 
 @pytest.fixture
 def first_product():
     return Product("Сыр", "Сыр с плесенью", 100.50, 3)
 
+
 @pytest.fixture
 def for_category():
-    return Category(
-        name="Молочные продукты",
-        description="Сыр с плесенью",
-        products = ["Сыр 1", "Сыр 2", "Сыр 3"]
-    )
+    return Category(name="Молочные продукты", description="Сыр с плесенью", products=["Сыр 1", "Сыр 2", "Сыр 3"])
+
 
 @pytest.fixture
 def for_category_empty_product():
-    return Category(
-        name="Молочные продукты",
-        description="Сыр с плесенью"
-    )
+    return Category(name="Молочные продукты", description="Сыр с плесенью")
