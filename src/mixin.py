925a10a0f278510fc87e6b46ef4aa30269b014c4
Daniil Tarasov <danya.tarasov.01@mail.ru>
2024-11-21 21:41:59 +0300

    sml fxs

--- src/mixin.py
+++ src/mixin.py
@@ -3,5 +3,4 @@ class ReprMixin:
         print(repr(self))
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})'
-
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
